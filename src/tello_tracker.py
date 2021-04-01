57ebf3d19a1bb1d258cf2fc27ef47e144b1383a1
Ricardo <ricardorico0527@gmail.com>
2021-04-01 11:06:23 +0200

    actions

--- src/tello_tracker.py
+++ src/tello_tracker.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 import rospy
-from std_msgs.msg import Empty
+from std_msgs.msg import Empty, UInt8
 from geometry_msgs.msg import Twist
 from projet.srv import CustomService, CustomServiceRequest
 
@@ -8,9 +8,13 @@ rospy.init_node('tello_tracker_node')
 
 pub_takeoff = rospy.Publisher('/tello/takeoff', Empty, queue_size=1)
 pub_land = rospy.Publisher('/tello/land', Empty, queue_size=1)
+pub_emergency = rospy.Publisher('/tello/emergency', Empty, queue_size=1)
+pub_flattrim = rospy.Publisher('/tello/flattrim', Empty, queue_size=1)
+pub_flip = rospy.Publisher('/tello/flip', UInt8, queue_size=1)
 pub_vel = rospy.Publisher('/tello/cmd_vel', Twist, queue_size=1)
 
 empty_msg = Empty()
+flip_msg = UInt8()
 vel_msg = Twist()
 
 rospy.wait_for_service('/track_to_vel_service')
@@ -20,36 +24,84 @@ call = CustomServiceRequest()
 work_rate = 10
 seconds_to_land = 10
 seconds_to_takeoff = 5
-in_air = False
+action_done = False
 count = 0
-
+orbit = False
+bounce = False
+start = rospy.get_time()
 print("Tello Tracker Initiated")
-while not rospy.is_shutdown():
-    rate = rospy.Rate(work_rate)
-    result = vel_service(call)   
-    if not in_air and result.tracking and count >= work_rate*seconds_to_takeoff:
-        print("Taking Off")
-        rate = rospy.Rate(1)
-        for i in range(5):
-            pub_takeoff.publish(empty_msg)
-            rate.sleep()
-        count = 0
-        in_air = True
-    if in_air and not result.tracking and count >= work_rate*seconds_to_land:
-        print("Landing")
-        rate = rospy.Rate(1)
-        for i in range(5):
-            pub_land.publish(empty_msg)
-            rate.sleep()
-        count = 0
-        in_air = False
-    if (in_air and not result.tracking) or (not in_air and result.tracking):
-        count += 1
-    elif in_air and result.tracking:
-        count = 0
-    
-    if count%work_rate == 0 and count != 0:
-            print("time elapsed counting: {}".format(count/work_rate))
+rate = rospy.Rate(work_rate)
+while not rospy.is_shutdown():    
+    result = vel_service(call)
     vel_msg = result.twist
+    if result.tracking:
+        start = rospy.get_time()
+        if result.id == 0:
+            pass
+        if result.id == 1 and not action_done:
+            flip_msg.data = 0
+            for i in range(3):
+                pub_flip.publish(flip_msg)
+                rospy.sleep(1)
+            #print("Flipping")
+        if result.id == 2 and not action_done:
+            for i in range(3):
+                pub_takeoff.publish(empty_msg)
+                rospy.sleep(1)
+            #print("Taking Off")
+        if result.id == 3:
+            #print("Tracking")
+            pass
+        if result.id == 4:
+            for i in range(3):
+                pub_land.publish(empty_msg)
+                rospy.sleep(1)
+            #print("Landing")
+        if result.id == 5:
+            for i in range(10):
+                pub_emergency.publish(empty_msg)
+                rospy.sleep(1)
+            #print("Emergency")
+        if result.id == 6:
+            vel_msg.linear.x = 0.5
+            vel_msg.angular.z = -0.8
+            orbit = True
+            #print("Orbiting")
+        if result.id == 7:
+            vel_msg.linear.x = 0
+            vel_msg.linear.y = 0
+            vel_msg.linear.z = 0
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = -1
+            vel_msg.angular.z = 0
+            orbit = False
+            bounce = False
+            #print("Stopping")
+        if result.id == 8:
+            vel_msg.linear.x = 0
+            vel_msg.linear.y = 0
+            vel_msg.linear.z = -3
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = 0
+            vel_msg.angular.z = 0
+            bounce = True
+            #print("Stopping")
+    else:
+        elapsed = rospy.get_time() - start
+        if elapsed > 2 and not orbit and not bounce:
+            vel_msg.linear.x = 0
+            vel_msg.linear.y = 0
+            vel_msg.linear.z = 0
+            vel_msg.angular.x = 0
+            vel_msg.angular.y = 0
+            vel_msg.angular.z = 0
+        elif orbit and elapsed > 10:
+            orbit = False
+        elif bounce and elapsed > 3 and elapsed < 5:
+            vel_msg.linear.z = 3
+        elif bounce and elapsed > 5:
+            bounce = False
+
+
     pub_vel.publish(vel_msg)
     rate.sleep()
--- src/track_to_vel_service.py
+++ src/track_to_vel_service.py
@@ -5,9 +5,6 @@ from projet.srv import CustomService, CustomServiceResponse
 from ar_track_alvar_msgs.msg import AlvarMarkers
 from tf.transformations import euler_from_quaternion
 from constants import LINEAR_THRESHOLD,ANGULAR_THRESHOLD,WEIGHT_LATERAL,WEIGHT_ALTITUDE,WEIGHT_DISTANCE,WEIGHT_ANGULAR_TURN,WEIGHT_ANGULAR_LATERAL,MIN_DISTANCE,MIN_HEIGHT
-
-
-
 class Service:
     def __init__(self):
         self.sub = rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.callback)
@@ -19,6 +16,7 @@ class Service:
         self.q_y = 0
         self.q_z = 0
         self.q_w = 0
+        self.id = 100
         self.tracking = False
         print("service server created")
 
@@ -32,29 +30,32 @@ class Service:
         self.my_response.twist.angular.y = 0
         self.my_response.twist.angular.z = 0
         self.my_response.tracking = False
+        self.my_response.id = 100    
         
         if self.tracking:
             self.tracking = False
+            self.my_response.id = self.id
             self.my_response.tracking = True
             
-            quaternion = (self.q_x,self.q_y,self.q_z,self.q_w)
-            euler = euler_from_quaternion(quaternion)
+            if self.id == 3:
+                quaternion = (self.q_x,self.q_y,self.q_z,self.q_w)
+                euler = euler_from_quaternion(quaternion)
 
-            l_x = self.p_x * WEIGHT_LATERAL #side
-            l_y = -self.p_y* WEIGHT_ALTITUDE #altitude
-            l_z = self.p_z * WEIGHT_DISTANCE #distance to marker
+                l_x = self.p_x * WEIGHT_LATERAL #side
+                l_y = -self.p_y* WEIGHT_ALTITUDE #altitude
+                l_z = self.p_z * WEIGHT_DISTANCE #distance to marker
 
-            a_z = euler[1] #rotation
+                a_z = euler[1] #rotation
 
-            if abs(l_x) > LINEAR_THRESHOLD:
-                self.my_response.twist.linear.x = l_x
-            if abs(l_z) > LINEAR_THRESHOLD:
-                self.my_response.twist.linear.y = l_z - MIN_DISTANCE
-            if abs(l_y) > LINEAR_THRESHOLD:
-                self.my_response.twist.linear.z = l_y + MIN_HEIGHT
-            if abs(euler[1]) > ANGULAR_THRESHOLD:
-                self.my_response.twist.angular.z = a_z * WEIGHT_ANGULAR_TURN
-                self.my_response.twist.linear.x += -a_z* WEIGHT_ANGULAR_LATERAL
+                if abs(l_x) > LINEAR_THRESHOLD:
+                    self.my_response.twist.linear.x = l_x
+                if abs(l_z) > LINEAR_THRESHOLD:
+                    self.my_response.twist.linear.y = l_z - MIN_DISTANCE
+                if abs(l_y) > LINEAR_THRESHOLD:
+                    self.my_response.twist.linear.z = l_y + MIN_HEIGHT
+                if abs(euler[1]) > ANGULAR_THRESHOLD:
+                    self.my_response.twist.angular.z = a_z * WEIGHT_ANGULAR_TURN
+                    self.my_response.twist.linear.x += -a_z* WEIGHT_ANGULAR_LATERAL
 
         return self.my_response
 
@@ -68,6 +69,7 @@ class Service:
             self.q_y = msg.markers[0].pose.pose.orientation.y
             self.q_z = msg.markers[0].pose.pose.orientation.z
             self.q_w = msg.markers[0].pose.pose.orientation.w
+            self.id = msg.markers[0].id
         else:
             self.p_x = 0
             self.p_y = 0
@@ -76,6 +78,7 @@ class Service:
             self.q_y = 0
             self.q_z = 0
             self.q_w = 0
+            self.id = 100
 
 rospy.init_node('track_to_vel_service_node')
 ss = Service()
